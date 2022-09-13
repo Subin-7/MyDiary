f21fde5e237c7c01ce9cd73d6b1e82df07851dcd
Subin-7 <lowerbin@naver.com>
2022-09-14 03:47:55 +0900

    Button click function, validating input fields

--- app/src/main/java/com/example/mydiary/DiaryDetailActivity.java
+++ app/src/main/java/com/example/mydiary/DiaryDetailActivity.java
@@ -3,15 +3,105 @@ package com.example.mydiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DiaryDetailActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
-    /**
-     * 상세보기 화면 or 작성하기 화면
-     */
+/**
+ * 상세보기 화면 or 작성하기 화면
+ */
+
+public class DiaryDetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTvDate;   // 일시 설정 텍스트
+    private EditText mEtTitle , mEtContent;  // 일기 제목 , 일기 내용
+    private RadioGroup mRgWeather;
+
+    private String mDetailMode = "";     //intent로 받아낸 게시글 모드
+    private String mBeforeDate ="";     // intent로 받아낸 게시글 기존 작성일자
+    private String mSelectedUserDate = ""; //선택 된 일시 값
+    private int mSelectedWeatherType = -1; // 선택 된 날씨 값 (1~6)
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_detail);
+
+        mTvDate = findViewById(R.id.tv_date);   //일시 설정 텍스트
+        mEtTitle = findViewById(R.id.et_title); //제목 입력 필드
+        mEtContent = findViewById(R.id.et_content); //내용 입력 필드
+        mRgWeather = findViewById(R.id.rg_weather); //날씨 선택 라디오 그룹
+
+        ImageView iv_back = findViewById(R.id.iv_back); //뒤로가기 버튼
+        ImageView iv_check = findViewById(R.id.iv_check);   //작성 완료 버튼
+
+
+        mTvDate.setOnClickListener(this);   // 클릭기능 부여
+        iv_back.setOnClickListener(this);   // 클릭기능 부여
+        iv_check.setOnClickListener(this);  // 클릭 기능 부여
+
+        //기본으로 설정 된 날짜의 갓을 지정 (디바이스 현재 시간 기준)
+        mSelectedUserDate = new SimpleDateFormat("yyyy/MM/dd E요일", Locale.KOREAN).format(new Date());
+        mTvDate.setText(mSelectedUserDate);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        // setOnClickListener가 붙어있는 뷰들은 클릭이 발생하면 모두 이곳을 수행하게 된다.
+        switch (view.getId()) {
+            case R.id.iv_back:
+                //뒤로가기 버튼
+                finish();   // 현재 엑티비티 종료
+                break;
+
+            case R.id.iv_check:
+                //작성 완료 버튼
+
+
+                //체크 값에 대해 아이디를 추출해내어 그 아이디 값을 WeatherType에 넣어준다.(라디오 그룹의 버튼 클릭 현재 상황 가지고 오기)
+                mSelectedWeatherType = mRgWeather.indexOfChild(findViewById(mRgWeather.getCheckedRadioButtonId()));
+
+                //입력 필드 작성란이 비어 있는지 체크
+                if (mEtTitle.getText().length() == 0 || mEtContent.getText().length() == 0){
+                    //error
+                    Toast.makeText(this, "제목과 내용을 모두 입력하여 주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // 날씨 선택이 되어 있는지 체크
+                if(mSelectedWeatherType == -1) {
+                    //error
+                    Toast.makeText(this, "날씨를 선택해 주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //데이터 저장
+                String title = mEtTitle.getText().toString();   // 제목 입력 값
+                String content = mEtContent.getText().toString();   //내용 입력 값
+                String userDate = mSelectedUserDate;    // 사용자가 선택한 일시
+
+                String writeDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.KOREAN).format(new Date());
+
+                // 데이터 베이스에 저장
+
+                finish(); //현재 액티비티 종료
+                break;
+
+            case R.id.tv_date:
+                // 일시 설정 텍스트
+
+                break;
+        }
     }
 }
